66b736535f81c2f917a457164c28afeebfa9d6df
miillaadddddd <milad7212@gmail.com>
2022-12-18 10:48:07 +0330

    Add navLinks component

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import NavLinks from "./NavLinks";
 function Header() {
   return (
     <header>
@@ -11,13 +12,20 @@ function Header() {
         </Link>
         <div className="flex items-center justify-end space-x-2">
           {/* DarkModeButton */}
-          <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 
+          <button
+            className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 
           
-            rounded-full dark:bg-stone-800 py-2 lg:py-4">
+            rounded-full dark:bg-stone-800 py-2 lg:py-4"
+          >
             Subscribe Now
           </button>
         </div>
       </div>
+      {/* NavLinks */}
+      <NavLinks/>
+
+      {/* SearchBox */}
+
     </header>
   );
 }
---
+++ app/NavLink.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+type Props = {
+  category: string;
+  isActive: boolean;
+};
+function NavLink({ category, isActive }: Props) {
+  return (
+    <Link
+      className={`navLink
+    ${
+      isActive &&
+      "underline decoration-orange-400 underline-offset-4 font-bold text-lg"
+    }
+    `}
+      href={`/news/${category}`}
+    >
+      {category}
+    </Link>
+  );
+}
+
+export default NavLink;
