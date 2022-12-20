27d8977e275bb0bcef4f85ccafabcce9544fda66
miillaadddddd <milad7212@gmail.com>
2022-12-20 17:23:41 +0330

    Add Alerts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,56 @@
-import React from 'react'
-import BlogPost from '../components/BlogPost'
-import Card from '../components/Card'
-import Card2 from '../components/Card2'
-import Card3 from '../components/Card3'
-import Card4 from '../components/Card4'
-import Cards from '../components/Cards'
-import Cards2 from '../components/Cards2'
-import ChatCard from '../components/ChatCard'
-import Input from '../components/Input'
-import Input2 from '../components/Input2'
-import Input3 from '../components/Input3'
-import Input4 from '../components/Input4'
-import Pricing from '../components/Pricing'
-import Row from '../components/Row'
-import Tabel1 from '../components/Tabel1'
+import React from "react";
+import Alert from "../components/Alert";
+import Alert2 from "../components/Alert2";
+import Alert3 from "../components/Alert3";
+import Alert4 from "../components/Alert4";
+import BlogPost from "../components/BlogPost";
+import Card from "../components/Card";
+import Card2 from "../components/Card2";
+import Card3 from "../components/Card3";
+import Card4 from "../components/Card4";
+import Cards from "../components/Cards";
+import Cards2 from "../components/Cards2";
+import Cards3 from "../components/Cards3";
+import ChatCard from "../components/ChatCard";
+import Footer from "../components/Footer";
+import Input from "../components/Input";
+import Input2 from "../components/Input2";
+import Input3 from "../components/Input3";
+import Input4 from "../components/Input4";
+import Pricing from "../components/Pricing";
+import Row from "../components/Row";
+import Tabel1 from "../components/Tabel1";
 
 function index() {
   return (
     <div className="">
-      <BlogPost/>
-      <Card/>
-      <Card2/>
-      <Card3/>
-      <Card4/>
-      <Cards/>
-      <Cards2/>
-      <Input/>
-      <Input2/>
-      <Input3/>
-      <Input4/>
-      <Row/>
-        <Tabel1/>
-        <ChatCard/>
-        <BlogPost/>
-        <Pricing/>
+      <Alert/>
+      <Alert2/>
+      <Alert3/>
+      <Alert4/>
+      <BlogPost />
+      <Card />
+      <Card2 />
+      <Card3 />
+      <Card4 />
+      <Cards />
+      <Cards2 />
+      <Cards3 />
+      <ChatCard />
+      <Footer/>
+
+
+      <Input />
+      <Input2 />
+      <Input3 />
+      <Input4 />
+
+      <Row />
+      <Tabel1 />
+      <BlogPost />
+      <Pricing />
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
