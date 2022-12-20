714ffe9152041c3c5186c14f481165fce304fd89
miillaadddddd <milad7212@gmail.com>
2022-12-20 17:51:41 +0330

    Add Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,13 +21,16 @@ import Input from "../components/Input";
 import Input2 from "../components/Input2";
 import Input3 from "../components/Input3";
 import Input4 from "../components/Input4";
+import Navbar from "../components/Navbar";
+import Navbar2 from "../components/Navbar2";
+import Navbar3 from "../components/Navbar3";
 import Pricing from "../components/Pricing";
 import Row from "../components/Row";
 import Tabel1 from "../components/Tabel1";
 
 function index() {
   return (
-    <div className="">
+    <div className="bg-gray-100">
       <Alert/>
       <Alert2/>
       <Alert3/>
@@ -52,6 +55,10 @@ function index() {
       <Input2 />
       <Input3 />
       <Input4 />
+
+      <Navbar/>
+      <Navbar2/>
+      <Navbar3/>
 
       <Row />
       <Tabel1 />
